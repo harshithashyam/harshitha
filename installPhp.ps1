71500c9a710aacf66168755e105679bfb9da1608
harshithashyam <50827467+harshithashyam@users.noreply.github.com>
2022-05-10 14:22:52 +0530

    Update installPhp.ps1

--- installPhp.ps1
+++ installPhp.ps1
@@ -4,6 +4,6 @@ $source = 'https://windows.php.net/downloads/releases/php-8.1.5-Win32-vs16-x64.z
 $destination = 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64.zip'
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination
-Mkdir 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
-Expand-Archive $destination -DestinationPath 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
+#Mkdir 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
+#Expand-Archive $destination -DestinationPath 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
 #Copy-item -Force -Recurse -Verbose 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-nts-Win32-vs16-x64\php-ini-development' -Destination 'C:\Users\harshitha.pechetti\Folder'

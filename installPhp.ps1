c35b3c2d10cc8ad2795661aaeacffbb10f90604f
harshithashyam <50827467+harshithashyam@users.noreply.github.com>
2022-05-12 11:49:43 +0530

    Update installPhp.ps1

--- installPhp.ps1
+++ installPhp.ps1
@@ -4,11 +4,11 @@ $source = 'https://windows.php.net/downloads/releases/php-8.1.5-Win32-vs16-x64.z
 $destination = 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64.zip'
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination
-Mkdir 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
+<#Mkdir 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
 Expand-Archive $destination -DestinationPath 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
 Copy-item -Force -Recurse -Verbose 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64\php.ini-development' -Destination 'C:\Users\harshitha.pechetti\Folder'
 #Copy-Item 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64\php.ini-development' 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64\php.ini'
 Rename-Item -Path 'C:\Users\harshitha.pechetti\Folder\php.ini-development' -NewName 'php.ini'
 Move-Item -Path 'C:\Users\harshitha.pechetti\Folder\php.ini' -Destination 'C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64'
 $Env:PATH += ";C:\Users\harshitha.pechetti\Folder\php-8.1.5-Win32-vs16-x64"
-Write-Output $Env:PATH
+Write-Output $Env:PATH#>

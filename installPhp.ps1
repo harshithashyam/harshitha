d891760adc34a621e52fc6b230971c547875b2df
harshithashyam <50827467+harshithashyam@users.noreply.github.com>
2022-05-13 10:29:15 +0530

    Update installPhp.ps1

--- installPhp.ps1
+++ installPhp.ps1
@@ -1,6 +1,10 @@
-# Source file location
-$source = 'https://github.com/harshithashyam/harshitha/blob/main/php-7.4.29-nts-Win32-vc15-x86.zip?raw=true'
-# Destination to save the file
-$destination = 'C:\Users\harshitha\Downloads\php-7.zip'
-#Download the file
-Invoke-RestMethod -Uri $source -OutFile $destination
+$Url = 'https://github.com/harshithashyam/harshitha/blob/main/php-7.4.29-nts-Win32-vc15-x86.zip?raw=true' 
+$ZipFile = 'C:\ZipFolder\' + $(Split-Path -Path $Url -Leaf) 
+$Destination= 'C:\Extracted\' 
+ 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+ 
+$ExtractShell = New-Object -ComObject Shell.Application 
+$Files = $ExtractShell.Namespace($ZipFile).Items() 
+$ExtractShell.NameSpace($Destination).CopyHere($Files) 
+Start-Process $Destination

33e736c4a61eb676b403c70e4c6b8b92fa930dd3
harshithashyam <50827467+harshithashyam@users.noreply.github.com>
2022-05-16 12:36:53 +0530

    Update installPhp.ps1

--- installPhp.ps1
+++ installPhp.ps1
@@ -1,10 +1,4 @@
-$Url = 'https://github.com/harshithashyam/harshitha/blob/main/php-7.4.29-nts-Win32-vc15-x86.zip?raw=true' 
-$ZipFile = 'C:\ZipFolder\' + $(Split-Path -Path $Url -Leaf) 
-$Destination= 'C:\Extracted\' 
- 
-Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
- 
-$ExtractShell = New-Object -ComObject Shell.Application 
-$Files = $ExtractShell.Namespace($ZipFile).Items() 
-$ExtractShell.NameSpace($Destination).CopyHere($Files) 
-Start-Process $Destination
+$source = 'https://github.com/harshithashyam/harshitha/blob/main/php-7.4.29-nts-Win32-vc15-x86.zip?raw=true' 
+$destination = 'C:\Users\harshitha\Downloads\php-7.zip'
+#Download the file
+Invoke-RestMethod -Uri $source -OutFile $destination

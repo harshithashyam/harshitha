2cde3e0b8e9153c8efb3c831b5a9c93419733b6d
harshithashyam <50827467+harshithashyam@users.noreply.github.com>
2022-05-16 13:24:54 +0530

    Update installPhp.ps1

--- installPhp.ps1
+++ installPhp.ps1
@@ -1,4 +1,4 @@
 $source = 'https://github.com/harshithashyam/harshitha/blob/main/php-7.4.29-nts-Win32-vc15-x86.zip?raw=true' 
-$destination = 'C:\Users\harshitha\Downloads\php-7.zip'
+$destination = 'D:\php-7.zip'
 #Download the file
 Invoke-RestMethod -Uri $source -OutFile $destination
